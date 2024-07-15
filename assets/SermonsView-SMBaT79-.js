01b511bc8e555f4f246527a3b362b1f3c409110c
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-07-15 17:31:17 +0200

    Shop Pagination working

---
+++ assets/SermonsView-SMBaT79-.js
@@ -0,0 +1,1 @@
+import{d as e,o,g as a,h as n}from"./index-DiYWzkFX.js";const c=e({__name:"SermonsView",setup(s){return(r,t)=>(o(),a(n))}});export{c as default};
--- assets/ShopView-DrSg3fnZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-BCor-Ud9.js";const n={};function r(t,s){return c(),o("div",null,"Shop")}const a=e(n,[["render",r]]);export{a as default};
